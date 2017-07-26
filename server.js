716b4f6a1af5371b1f1355fc9b876d16c51c8f82
Gavin Stark <gavin@gstark.com>
2017-07-26 10:15:25 -0400

    Hello world using mustache templates

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
+// Using the express library
 const express = require('express')
+// Using the mustache express library
+const mustacheExpress = require('mustache-express')
 
+// Build the express app
 const app = express()
 
+// Teach our app to use the mustache engine for rendering templates
+// rendering = "draw" our HTML
+app.engine('mst', mustacheExpress())
+
+// Teach our app what directory to f\ind our views (templates) live
+app.set('views', './views')
+
+// Teach our app to use mustache for our templates
+app.set('view engine', 'mst')
+
 app.get('/', (request, response) => {
-  response.send('Hello, World. We are reloading!')
+  // sending content to the browser from javascript land
+  // response.send('Hello, World. We are reloading!')
+
+  response.render('home')
 })
 
 app.listen(3000, () => {
   console.log('Hooray, our app is listening on port 3000')
 })
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
